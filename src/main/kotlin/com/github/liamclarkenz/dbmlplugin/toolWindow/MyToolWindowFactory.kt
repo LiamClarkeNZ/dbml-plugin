57fe13ff73ca84c60dabad3f6db8f3bef8aaf884
GitHub Action <action@github.com>
2026-03-18 22:51:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/liamclarkenz/dbmlplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/liamclarkenz/dbmlplugin/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.liamclarkenz.dbmlplugin.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.liamclarkenz.dbmlplugin.MyBundle
+import com.github.liamclarkenz.dbmlplugin.services.MyProjectService
 import javax.swing.JButton
 
 
